2eb41440ee523cad9d9517df22ca123255ed2c9d
safae-cmd <safaealamimchichi@gmail.com>
2023-03-16 15:36:58 -0700

    trailing whitespace removed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-	
+int main(void)
+{
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
-
-	puts (str);
+	puts(str);
 
         return 0;
 }	
