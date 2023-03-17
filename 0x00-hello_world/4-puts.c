c827d365c5e36406012feed1b9e9b89029830336
safae-cmd <safaealamimchichi@gmail.com>
2023-03-16 18:07:15 -0700

    t

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 
-int main(void)
+
+int main()
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
-
 	puts(str);
 
-        return 0;
+
+        return (0);
 }	
