b6ba0ebe280653740954201f4131e64ee9c5825f
safae-cmd <safaealamimchichi@gmail.com>
2023-03-17 19:37:03 -0700

    t

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - base 10
+ * Return: decimal starting from (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
