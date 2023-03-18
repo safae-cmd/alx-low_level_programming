0a75de2312a67805dd660887fb1b7dca056e051c
safae-cmd <safaealamimchichi@gmail.com>
2023-03-17 19:15:45 -0700

    t

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - alphabet
+ * Return: lower upper
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
