4e0b8a909efedd9ad5a404c37c64bf5a06a26449
safae-cmd <safaealamimchichi@gmail.com>
2023-03-19 18:19:57 -0700

    t

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,16 +4,22 @@
  * Return: (0)
  */
 int main(void)
-{	char i;
+{
+	char i;
 	int j;
 	long int k;
 	long long int l;
 	float m;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
+
 	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(j));
+
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(m));
+
 	return (0);
 }
