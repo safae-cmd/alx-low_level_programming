d1db971aa5b681cf7232c2a92b7bbfe09212bd55
safae-cmd <safaealamimchichi@gmail.com>
2023-03-20 19:32:23 -0700

    r

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,24 +7,25 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar('0' + i / 10);
-
-
-		for (j = i + 1; j <= 99; j++)
-		{
-			putchar('0' + i / 10);
-			putchar('0' + i % 10);
-			putchar(',');
-			putchar(' ');
-			putchar('0' + j / 10);
-			putchar('0' + j % 10);
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
 	return (0);
 }
-
