03cc6548f9a29cccb18fc4ce21047f5513544703
safae-cmd <safaealamimchichi@gmail.com>
2023-03-23 15:53:26 -0700

    compilrd task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
