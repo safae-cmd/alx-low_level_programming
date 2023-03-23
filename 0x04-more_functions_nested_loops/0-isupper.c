97eb8138accbd539eb78ed353dc7f5ed04b3b010
safae-cmd <safaealamimchichi@gmail.com>
2023-03-23 15:24:29 -0700

    compilrd task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _isupper - checks for uppercase
@@ -15,4 +16,20 @@ int _isupper(int c)
 	{
 		return (0);
 	}
+}
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
