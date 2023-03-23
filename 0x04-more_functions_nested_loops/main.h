ba0a528167d3aed9192629b7a7ca571092ae3a71
safae-cmd <safaealamimchichi@gmail.com>
2023-03-23 15:08:35 -0700

    added readme + code task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _putchar(char c);
+
+/**
+ *  * _putchar - writes the character c to stdout
+ *   * @c: The character to print
+ *    *
+ *     * Return: On success 1.
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *       */
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
+
