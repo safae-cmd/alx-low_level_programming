94899ef1cc27da34f1f777feb122946d1a71c3dc
safae-cmd <safaealamimchichi@gmail.com>
2023-03-24 04:55:32 -0700

    rectified code in task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw diagonal line
+ * n@: parameter
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int j, i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (j = 1; j <= n; j++)
+	{
+		for (i = 1; i <= j; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
