2281e7e43ab53a729b9efaee521a9c6a3aedec25
safae-cmd <safaealamimchichi@gmail.com>
2023-03-24 09:52:11 -0700

    tested case 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
 	while (i < 10)
 	{
+		/**
+		 * loop is not incremented
+		 */
 		putchar(i);
 	}
-
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
