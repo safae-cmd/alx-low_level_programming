4829aed37884421ca89d04a99013ebf6e6c5960b
safae-cmd <safaealamimchichi@gmail.com>
2023-03-25 10:33:48 -0700

    modified task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,17 @@ int main(void)
 		if (i % 3 == 0)
 		{
 			printf("Fizz ");
+			continue;
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
+			continue;
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
+			continue;
 		}
 		else
 		{
