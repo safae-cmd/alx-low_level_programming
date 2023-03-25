9e174a9f747251cf6c1532dff450b134fa13dfdd
safae-cmd <safaealamimchichi@gmail.com>
2023-03-25 08:25:09 -0700

    modified

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,14 +13,14 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
+			if (j >= 10)
+			{
+				_putchar('1');
+				_putchar((j % 10) + '0');
+			}
+			else
 			{
 				_putchar(j + '0');
-			}
-			else if (j >= 10)
-			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
 			}
 		}
 	write(1, "\n", 2);
