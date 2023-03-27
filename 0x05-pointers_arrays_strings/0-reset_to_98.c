011a6c987a4826d510c785fcab50854bd98ed256
safae-cmd <safaealamimchichi@gmail.com>
2023-03-27 11:12:14 -0700

    code task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset variable value to 98 through dereferencing
+ * @n: pointer to an int
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
