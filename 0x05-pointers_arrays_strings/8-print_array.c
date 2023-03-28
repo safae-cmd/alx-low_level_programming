9d83d14e914d9133b76132fe17c65524cd1390f6
safae-cmd <safaealamimchichi@gmail.com>
2023-03-28 16:42:19 -0700

    added code task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array
+ * @a: int
+ * @n: int
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
