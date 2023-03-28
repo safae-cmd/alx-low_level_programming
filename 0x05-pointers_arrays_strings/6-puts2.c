dea0faae219bae826ebabdfaa115da1fd3d1e6f9
safae-cmd <safaealamimchichi@gmail.com>
2023-03-28 11:39:26 -0700

    modified task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,13 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0' && str[i + 1] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
 		_putchar(str[i]);
+		if (str[i + 1] == '\0')
+		{
+			break;
+		}
 	}
 	_putchar('\n');
 }
