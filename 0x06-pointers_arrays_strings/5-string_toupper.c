22075faa1359144a3ba7c158ff308833219ec2e7
safae-cmd <safaealamimchichi@gmail.com>
2023-03-30 16:14:05 -0700

    modified code task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,15 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * *string_toupper - change lowercase to uppercase
- * @*c: char
+ * string_toupper - change lowercase to uppercase
+ * @c: char
  * Return: string in uppercase
  */
 char *string_toupper(char *c)
 {
-	int i, n;
+	int i;
 
-	n = strlen(c);
-
-	for (i = 0; i < n; i++)
+	for (i = 0; c[i] != '\0'; i++)
 	{
 		if (c[i] <= 122 && c[i] >= 97)
 		{
