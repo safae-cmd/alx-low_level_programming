b4536d4419c6bb3d223b6d5e8d5f41209dc6cd8b
safae-cmd <safaealamimchichi@gmail.com>
2023-03-30 04:53:05 -0700

    added code for task 0 and the header file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int n;
+
+	n = strlen(dest);
+	dest[n] = ' ';
+	for (i = 0; i <= n; i++)
+	{
+		dest[n + i] = src[i];
+	}
+	return (&dest[n + i]);
+}
