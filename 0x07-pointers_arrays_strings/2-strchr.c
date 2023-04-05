06fc9d4332b75cd95799f36d9b8fc18712f50d30
safae-cmd <safaealamimchichi@gmail.com>
2023-04-05 04:28:39 -0700

    code modified for task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strchr - locate character in a string
+ * @s: string
+ * @c: character to locate
+ * Return: c or null
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
