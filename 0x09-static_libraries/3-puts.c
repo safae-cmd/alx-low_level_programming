ae11f3498abac297cf5151268cc85f2d4638a467
safae-cmd <safaealamimchichi@gmail.com>
2023-04-07 16:02:48 -0700

    modified _puts and updated library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
-void _puts(char *s) {
-    int i = 0;
-    while (s[i] != '\0') {
-        _putchar(s[i]);
-        i++;
-    }
+/**
+ * _puts - prints string
+ * @s: string
+ * Return: void
+ */
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
 	_putchar('\n');
 }
