77b83ebe693db73278ec391fef733d18b721165e
safae-cmd <safaealamimchichi@gmail.com>
2023-04-09 05:32:54 -0700

    updated header + code task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of command line arguments
+ * @argc: count
+ * @argv: string
+ * Return: 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 int main (int argc, char *argv[]);
+int main(int argc, char __attribute__((unused)) *argv[]);
