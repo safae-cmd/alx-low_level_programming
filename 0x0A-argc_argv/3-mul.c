96956c972d2959558550babce8c47cab27fc7196
safae-cmd <safaealamimchichi@gmail.com>
2023-04-09 11:11:05 -0700

    modified code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,12 +12,14 @@ int main(int argc, char *argv[])
 {
 	int a, b, c;
 
-	if (argc >= 3)
+	if (argc == 3)
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
 		c = a * b;
 		printf("%d\n", c);
 	}
+	else
+		printf("Error\n");
 	return (0);
 }
