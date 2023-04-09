995329c847ffe718281b1ab2cc0de2236305e718
safae-cmd <safaealamimchichi@gmail.com>
2023-04-09 05:22:59 -0700

    added code for task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * _name - prints name of the program
+ * main - prints name of the program
  * @argc: argument counter
  * @argv: string array
  * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc >= 1)
 		printf("%s\n", argv[0]);
