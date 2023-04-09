f93c9305695767a86027c1a439db353c099bdd67
safae-cmd <safaealamimchichi@gmail.com>
2023-04-09 05:51:49 -0700

    code task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: count
+ * @argv: string arg
+ * Return (0)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
