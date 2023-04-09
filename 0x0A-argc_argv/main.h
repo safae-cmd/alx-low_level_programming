90bee903c602367b25ad66776a9a376f58fc8960
safae-cmd <safaealamimchichi@gmail.com>
2023-04-09 05:54:59 -0700

    updated header

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
-int main (int argc, char *argv[]);
+int main(int argc, char *argv[]);
 int main(int argc, char __attribute__((unused)) *argv[]);
+int main(int argc, char *argv[]);
