18ecffe40e9e1cf3d33bc7634b9453cef86c71e9
safae-cmd <safaealamimchichi@gmail.com>
2023-04-11 16:44:26 -0700

    code task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_strdup - return pointer to new space of the copy of string
+ * @str: string
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	unsigned int i;
+	char *p;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (1)
+	{
+		p = (char *)malloc(strlen(str));
+		for (i = 0; i < strlen(str); i++)
+		{
+			p[i] = str[i];
+		}
+		return (p);
+	}
+	return (NULL);
+}
