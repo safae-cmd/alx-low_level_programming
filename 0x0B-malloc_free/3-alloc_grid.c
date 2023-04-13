1a37c330d54bb3558c02eddf15c641e1dcfb33e4
safae-cmd <safaealamimchichi@gmail.com>
2023-04-12 17:41:13 -0700

    code task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - return pointer to a 2 dimensional array of strings
+ * @width: width
+ * @height: height
+ * Return: int
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(int *) * height);
+		if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(sizeof(int) * (i * width));
+		if (p[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(p[i]);
+			}
+			free(p);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
