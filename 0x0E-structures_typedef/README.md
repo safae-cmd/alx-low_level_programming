4d4908f0dce64a00c4207803b4c225329b4597c8
safae-cmd <safaealamimchichi@gmail.com>
2023-04-18 04:15:09 -0700

    added readme + 2 headers

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+introduction to structures and typedef in C
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE_NAME_H
+#define HEADER_FILE_NAME_H
+/**
+ * struct dog - struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
