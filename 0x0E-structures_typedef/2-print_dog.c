0a0dfed875e5c3a645a8ab73c92ba245b8fcaf16
safae-cmd <safaealamimchichi@gmail.com>
2023-04-18 08:58:21 -0700

    main updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,38 +17,23 @@ void print_dog(struct dog *d)
 
 	i = 0;
 	while (name[i] != '\0')
-	{
 		i++;
-	}
 	d->name = malloc(sizeof(char) * (i + 1));
 	if (d->name == NULL)
-	{
 		printf("Name: (nil)\n");
-	}
 	else
-	{
 		printf("Name: %s\n", d->name);
 	d->age = age;
 	if (d->age == 0)
-	{
 		printf("Age: (nil)\n");
-	}
 	else
-	{
 		printf("Age: %f\n", d->age);
-	}
 	i = 0;
 	while (owner[i] != '\0')
-	{
 		i++;
-	}
-	d->owner = malloc(sizeof(char) * (i+ 1));
+	d->owner = malloc(sizeof(char) * (i + 1));
 	if (d->owner == NULL)
-	{
 		printf("Owner: (nil)\n");
-	}
 	else
-	{
 		printf("Owner: %s\n", d->owner);
-	}
 }
