ae0614b828d1895fe46be20c7dd4679ae4ef700a
safae-cmd <safaealamimchichi@gmail.com>
2023-04-19 14:19:45 +0000

    added code task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - executes function as a parameter
+ * @array: pointer to the array to be printed
+ * @size: size of array
+ * @action: pointer to print functions
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
