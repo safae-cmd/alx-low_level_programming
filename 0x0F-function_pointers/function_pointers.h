91b17e54faed4a436a5fcbe1a81a3da93d5c6a5d
safae-cmd <safaealamimchichi@gmail.com>
2023-04-19 13:29:43 +0000

    modified header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
+#ifndef POINTER_TO_FUNCTION_H
+#define POINTER_TO_FUNCTION_H
+void print_name_uppercase(char *name);
+void print_name_as_is(char *name);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -8,3 +11,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 int _putchar(char c);
+#endif
