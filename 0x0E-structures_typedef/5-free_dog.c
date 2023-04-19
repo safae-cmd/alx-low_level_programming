cacd14a8c8299e658565f46cbbea23e955ed75e7
safae-cmd <safaealamimchichi@gmail.com>
2023-04-18 20:03:41 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees memory
+ * @d: poiter to dog
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
