f87506b5c58e90045a190500d71e3e54bda431c1
safae-cmd <safaealamimchichi@gmail.com>
2023-04-22 02:15:23 +0000

    modofied task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -38,7 +38,7 @@ void print_all(const char * const format, ...)
 				break;
 		}
 		if (format[i + 1] != '\0' && (format[i] == 'c'
-				|| format[i] == 'i' || format[i] == 'f'))
+				|| format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
 		{
 			printf(", ");
 		}
