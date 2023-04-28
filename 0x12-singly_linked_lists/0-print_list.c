b76effe6346838b8f7bf396d5c4929fcfacbc672
safae-cmd <safaealamimchichi@gmail.com>
2023-04-28 20:47:59 +0000

    modified task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,19 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	if (h == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
 	i = 0;
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		i++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
