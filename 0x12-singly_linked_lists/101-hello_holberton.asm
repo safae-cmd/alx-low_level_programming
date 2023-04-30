2a2ba49daa9ebd9d565ff21546b42cbb1a87f85b
safae-cmd <safaealamimchichi@gmail.com>
2023-04-30 19:10:05 +0000

    task101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+	fmt: db "Hello, Holberton",10,0
+
+section .text
+	global main
+	extern printf
+
+main:
+	push rbp
+	mov rbp, rsp
+
+	mov edi, fmt
+	xor eax, eax
+	call printf
+
+	mov rsp, rbp
+	pop rbp
+	xor eax, eax
+	ret
