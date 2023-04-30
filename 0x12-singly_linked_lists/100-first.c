676c5a890cdd383cd984ddd4761209755661f9c9
safae-cmd <safaealamimchichi@gmail.com>
2023-04-30 18:47:55 +0000

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+void _printfunction(void)  __attribute__((constructor));
+
+/**
+ * _printfunction - prints text
+ * Return:void
+ */
+void _printfunction(void)
+{
+	list_t text = {"You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n", 35, NULL};
+	printf("%s", text.str);
+}
