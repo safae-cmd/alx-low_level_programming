6fe80b2da2c60fb24aa36acfaa7f493231e6307d
safae-cmd <safaealamimchichi@gmail.com>
2023-05-02 10:49:32 +0000

    modified task5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *p;
 
-	p = *head;
-	free(*head);
-	while (p)
+	if (!head)
 	{
-		p = p->next;
+		return;
+	}
+	while (*head)
+	{
+		p = *head;
+		*head = (*head)->next;
 		free(p);
 	}
+	*head = NULL;
 }
