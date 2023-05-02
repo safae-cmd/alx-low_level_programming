3c3204a986dc00d16dfc965e0cb2484409ee9de9
safae-cmd <safaealamimchichi@gmail.com>
2023-05-02 09:44:11 +0000

    modified task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
-	if (!h)
-	{
-		return (1);
-	}
 	while (h)
 	{
 		printf("%i\n", h->n);
-		h = h->next;
 		i++;
+		h = h->next;
 	}
 	return (i);
 }
