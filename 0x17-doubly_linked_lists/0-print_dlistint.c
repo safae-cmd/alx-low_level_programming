5fd9325d3e6d37c010182be9e719acf53648935c
safae-cmd <safaealamimchichi@gmail.com>
2023-06-15 18:56:06 +0000

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of a list
+ * @h: pointer to list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i;
+	const dlistint_t *p;
+	p = h;
+	i = 1;
+	while (p)
+	{
+		printf("%i\n", p->n);
+		i++;
+		p = p->next;
+	}
+	return (i);
+}
