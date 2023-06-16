1c6d33264dfde098f8bbe377959c11e95404d993
safae-cmd <safaealamimchichi@gmail.com>
2023-06-16 00:18:30 +0000

    task 3

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,23 +8,20 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    
-    if (!new_node)
-    {
-        return NULL;
-    }
-    
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
-    
-    if (*head)
-    {
-        (*head)->prev = new_node;
-    }
-    
-    *head = new_node;
-    
-    return new_node;
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head)
+	{
+		(*head)->prev = new;
+	}
+	*head = new;
+	return (new);
 }
